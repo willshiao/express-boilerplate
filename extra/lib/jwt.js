8bfd20b0a984488c116ac039c6d81ba7b707f6e1
William Shiao <willshiao@gmail.com>
2018-07-10 14:11:30 -0700

    Add express JWT module to jwt.js

--- extra/lib/jwt.js
+++ extra/lib/jwt.js
@@ -1,14 +1,16 @@
 'use strict'
 
-const config = require('config')
 const Promise = require('bluebird')
 const jwt = Promise.promisifyAll(require('jsonwebtoken'))
+const expressJwt = require('express-jwt')
+const config = require('./config')
 
 module.exports = {
   async sign (data) {
-    return jwt.signAsync({ data }, config.get('jwt.secret'), config.get('jwt.settings'))
+    return jwt.signAsync(data, config.get('jwt.secret'), config.get('jwt.settings'))
   },
   async verify (token) {
     return jwt.verify(token, config.get('jwt.secret'))
-  }
+  },
+  express: expressJwt({ secret: config.get('jwt.secret') })
 }
